b333b435ae7fb9b37831cbd332baf86cc948bb48
filblue <denis.fileev@gmail.com>
2014-11-24 13:15:08 +0200

    first commit

---
+++ UICatalog/AAPLAppDelegate.m
@@ -0,0 +1,29 @@
+/*
+ Copyright (C) 2014 Apple Inc. All Rights Reserved.
+ See LICENSE.txt for this sample’s licensing information
+ 
+ Abstract:
+ The application-specific delegate class.
+*/
+
+#import "AAPLAppDelegate.h"
+
+@interface AAPLAppDelegate() <UISplitViewControllerDelegate>
+@end
+
+@implementation AAPLAppDelegate
+
+#pragma mark - UIApplicationDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    return YES;
+}
+
+#pragma mark - UISplitViewControllerDelegate
+
+- (UISplitViewControllerDisplayMode)targetDisplayModeForActionInSplitViewController:(UISplitViewController *)splitViewController {
+    return UISplitViewControllerDisplayModeAllVisible;
+}
+
+@end
